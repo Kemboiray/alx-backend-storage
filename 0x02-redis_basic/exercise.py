c5bd48b152a4da1e16315fe9e4ceac8199d51152
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-19 01:36:04 +0300

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-"""
-This module defines a class `Cache` that stores and flushes an instance of the
-`Redis` client.
-"""
-from typing import Union
+"""This module defines a class `Cache` """
+from typing import Any, Callable, Optional, Union
 
 
 class Cache:
-    """
-    Public class method:
-        store - Store input data in `Redis` using a randomly generated key
-    """
+    """Handle storage and retrieval of data caches using `Redis` """
+
     def __init__(self) -> None:
         """Initialise `Redis` client """
         import redis
@@ -27,3 +22,31 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """Return the value of `key`, if it exists """
+        val = self._redis.get(key)
+        if val is not None:
+            if fn is not None:
+                return fn(val)
+            return val
+
+    def get_str(self, key: str) -> Any:
+        """Parameterize `Cache.get` with `fn`=`str` """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> Any:
+        """Parameterize `Cache.get` with `fn`=`int` """
+        return self.get(key, int)
+
+
+if __name__ == "__main__":
+    cache = Cache()
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
